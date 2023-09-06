7102654b802d98a511d3dbb6019a178cfee8b0e4
Ranoshis Das <127428253+RanoshisDas@users.noreply.github.com>
2023-09-06 22:50:18 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
-#Write a program to print numbers from 1 to 10
-for i in range(1,11):
+#Write a program to print numbers from lower limit to uper limit
+l=int(input('Enter lower limit'))
+u=int(input('Enter uper limit'))
+for i in range(l,u+1): #using u+1 because it will check condition upto u-1 times
     print(i,end=' ')
